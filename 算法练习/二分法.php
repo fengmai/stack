7188330a7d699b4cb30c2315f08ab889bfc92157
闫文 <yanwen@doweidu.com>
2020-07-10 09:30:12 +0800

    二分法

--- 算法练习/二分法.php
+++ 算法练习/二分法.php
@@ -1,31 +1,36 @@
 <?php
 
-
-
-function searchArray($array,$searchKey)
+//二分查找
+function binarySearch($array,$searchKey)
 {
-   $max =  count($array);
+   $count = count($array);
+   $low = 0;
+   if ($count ==0) return 0;
+   $height = $count-1;
 
-   if ($max<=0) {return  0;}
-   $min = 0;
-   while($min <= $max)
+
+
+   while ($low  <=$height)
    {
-      $mid = intval(($min+$max)/2);
+       $mid = intval(($low +$height)/2);
 
-      if ($array[$mid] == $searchKey)
-          return $mid;
+       if ($array[$mid] == $searchKey)
+       {
+         return  $mid;
+       }
 
-      if ($array[$mid] >$searchKey){
-          $max = $mid-1;
-      }else{
-          $min = $mid+1;
-      }
-
+       if ($array[$mid] >$searchKey){
+           $height = $mid -1;
+       }else{
+           $low  =  $mid +1;
+       }
    }
-   return  0;
-
+    return 0;
 }
 
+
 $arr = array(1,2,4,6,10,40,50,80,100,110);
-echo searchArray($arr,80);
+echo binarySearch($arr,80);
+
+
 
