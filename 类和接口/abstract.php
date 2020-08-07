0bcea83e2c6e08ee0bffccd49fd599e9ca8c40d5
闫文 <yanwen@doweidu.com>
2020-08-07 09:33:18 +0800

    demo

---
+++ 类和接口/abstract.php
@@ -0,0 +1,20 @@
+<?php
+
+abstract  class  father
+{
+
+   abstract public function  eat($name,$act);
+
+
+}
+
+
+class son extends  father
+{
+    public function eat($name, $act)
+    {
+        // TODO: Implement eat() method.
+    }
+
+
+}
